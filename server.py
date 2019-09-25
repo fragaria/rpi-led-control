bb1fe8557630f9c688ea94209f189b0ecf0d391f
Jirka Chadima <chadima.jiri@gmail.com>
2019-09-25 09:51:41 +0200

    Actually shows the LEDs after startup

--- server.py
+++ server.py
@@ -7,9 +7,10 @@ NUM_LED = int(os.environ.get('NUM_LED', 2))
 START_COLOR = int(os.environ.get('START_COLOR', 16711680))
 
 strip = apa102.APA102(num_led=NUM_LED, global_brightness=20, mosi=14, sclk=15, order="rbg")
+strip.clear_strip()
 for i in range(0, NUM_LED):
     strip.set_pixel_rgb(i, START_COLOR)
-strip.clear_strip()
+strip.show()
 
 class Leds(object):
     def on_post(self, req, res):
